68367c7100bea83082d74d11af5f857fa53449fb
a-babushkin <a.babushkin42@gmail.com>
2025-09-08 20:06:20 +0300

    Writing tests for Places

--- places/tests.py
+++ places/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from rest_framework_simplejwt.tokens import AccessToken
 
-# Create your tests here.
+from places.models import Place
+from users.models import User
+
+class PlaceViewSetTests(APITestCase):
+    def setUp(self):
+        """Начальные установки для тестирования"""
+        self.user = User.objects.create(email="user@mail.ru", is_staff=True)
+
+        self.access_token = str(AccessToken.for_user(self.user))
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
+
+        self.place1 = Place.objects.create(title='Place 1')
+        self.place2 = Place.objects.create(title='Place 2')
+
+    def test_list_places(self):
+        """Тестирование получения списка действий"""
+        response = self.client.get('/places/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Place.objects.count(), 2)
+
+    def test_create_place(self):
+        """Тестирование создания действия"""
+        data = {
+            'title': 'New Place',
+        }
+        response = self.client.post('/places/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Place.objects.count(), 3)
+        self.assertEqual(Place.objects.get(id=response.data['id']).title, 'New Place')
+
+    def test_update_place(self):
+        """Тестирование обновления действия"""
+        data = {
+            'title': 'Updated Place'
+        }
+        response = self.client.put(f'/places/{self.place1.id}/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.place1.refresh_from_db()
+        self.assertEqual(self.place1.title, 'Updated Place')
+
+    def test_delete_place(self):
+        """Тестирование удаления действия"""
+        response = self.client.delete(f'/places/{self.place1.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Place.objects.count(), 1)  # Должно остаться 1 действие
+
+    def test_unauthenticated_user_access(self):
+        """Тестирование доступа неаутентифицированного пользователя"""
+        self.client.logout()
+        response = self.client.get('/places/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)

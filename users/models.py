0191d6c4553d7c351ae2f447536dafbb3715d64f
a-babushkin <a.babushkin42@gmail.com>
2025-09-08 09:31:41 +0300

    Create model, migration, command

--- users/models.py
+++ users/models.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        verbose_name="E-mail", unique=True, help_text="Введите электронную почту"
+    )
+    avatar = models.ImageField(
+        verbose_name="Аватар",
+        upload_to="users/avatars/",
+        blank=True,
+        null=True,
+        help_text="Загрузите свое фото",
+    )
+    phone_number = models.CharField(
+        verbose_name="Телефон",
+        max_length=15,
+        blank=True,
+        null=True,
+        help_text="Введите номер телефона",
+    )
+    city = models.CharField(
+        verbose_name="Город",
+        max_length=30,
+        blank=True,
+        null=True,
+        help_text="Укажите город",
+    )
+    tg_chat_id = models.IntegerField(
+        verbose_name="Chat ID",
+        blank=True,
+        null=True,
+        help_text="Укажите chat ID в Телеграм",
+    )
+    tg_username = models.CharField(
+        verbose_name="Имя пользователя",
+        blank=True,
+        null=True,
+        help_text="Укажите Ник в Телеграм",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["email"]

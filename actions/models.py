ea4caffaf5ce0d56ba9ccf3047e42f581e6a9a79
a-babushkin <a.babushkin42@gmail.com>
2025-09-08 09:58:38 +0300

    Create model, migration

--- actions/models.py
+++ actions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Action(models.Model):
+    """Модель действия для закрепления привычки"""
+
+    title = models.CharField(
+        max_length=255,
+        verbose_name="Наименование",
+        help_text="Введите наименование действия, которое необходимо выполнить.",
+    )
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Действие"
+        verbose_name_plural = "Действия"
+        ordering = ["title"]

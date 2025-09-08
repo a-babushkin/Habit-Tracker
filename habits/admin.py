4423ce3f7e24295d8e8ebd614dc613b586c4afb3
a-babushkin <a.babushkin42@gmail.com>
2025-09-08 10:24:10 +0300

    Create serializers, paginations

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from habits.models import Habit, Place, Action
 
-# Register your models here.
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ("id", "time", "periodicity", "duration")
---
+++ habits/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+
+from actions.serializers import ActionTitleSerializer
+from habits.models import Habit
+from habits.validators import RewardOrRelatedHabitValidator, PleasantHabitRelatedValidator, \
+    NoRewardForPleasantHabitValidator, MaxDurationValidator, PeriodicityValidator
+from places.serializers import PlaceTitleSerializer
+
+
+
+# ===== Секция привычек ===============================================
+class HabitSerializer(serializers.ModelSerializer):
+    action = ActionTitleSerializer(read_only=True)
+    place = PlaceTitleSerializer(read_only=True)
+
+    class Meta:
+        model = Habit
+        fields = [
+            "id",
+            "owner",
+            "action",
+            "place",
+            "time",
+            "duration",
+            "is_pleasant",
+            "periodicity",
+            "is_public",
+
+        ]
+
+
+class HabitCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habit
+        fields = "__all__"
+        validators = [
+            RewardOrRelatedHabitValidator(),
+            PleasantHabitRelatedValidator(),
+            NoRewardForPleasantHabitValidator(),
+            MaxDurationValidator(),
+            PeriodicityValidator(),
+        ]
+
+
+class HabitPublicSerializer(serializers.ModelSerializer):
+    action = ActionTitleSerializer(read_only=True)
+    place = PlaceTitleSerializer(read_only=True)
+
+    class Meta:
+        model = Habit
+        fields = ("action", "is_pleasant", "time", "place")
+
+
+class HabitDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habit
+        fields = "__all__"
